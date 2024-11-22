2d5e7c6c9224245032ff333dbfeaea57018b978c
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-22 23:42:35 +0100

    Create and use createRepresentativeAction server action

--- src/app/representatives/representative-drawer.tsx
+++ src/app/representatives/representative-drawer.tsx
@@ -10,6 +10,8 @@ import {
   DrawerTrigger,
   Input,
 } from "@/components";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export function RepresentativeDrawer() {
   return (
@@ -32,7 +34,7 @@ export function RepresentativeDrawer() {
 
 function CreateRepresentativeForm() {
   return (
-    <Form action="" className="flex flex-col gap-2">
+    <Form action={createRepresentativeAction} className="flex flex-col gap-2">
       <Input type="text" name="firstName" placeholder="First Name" required />
       <Input type="text" name="lastName" placeholder="Last Name" required />
       <Input type="email" name="email" placeholder="Email" required />
@@ -42,4 +44,16 @@ function CreateRepresentativeForm() {
       </DrawerClose>
     </Form>
   );
+}
+
+async function createRepresentativeAction(formData: FormData) {
+  "use server";
+  const firstName = formData.get("firstName") as string;
+  const lastName = formData.get("lastName") as string;
+  const email = formData.get("email") as string;
+
+  console.log(`Creating representative: ${firstName} ${lastName} (${email})`);
+
+  revalidatePath("/representatives");
+  redirect("/representatives");
 }
