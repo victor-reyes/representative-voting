ef3ec38160b27680de68262b005d1cd237d83d24
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-22 23:28:15 +0100

    Add CreateRepresentativeForm with first and last names and email fields

--- src/app/representatives/representative-drawer.tsx
+++ src/app/representatives/representative-drawer.tsx
@@ -1,8 +1,11 @@
+import Form from "next/form";
 import {
   Button,
   Drawer,
+  DrawerClose,
   DrawerContent,
   DrawerTrigger,
+  Input,
 } from "@/components";
 
 export function RepresentativeDrawer() {
@@ -13,9 +16,23 @@ export function RepresentativeDrawer() {
       </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm p-4">
-          Add a new representative
+          <CreateRepresentativeForm />
         </div>
       </DrawerContent>
     </Drawer>
+  );
+}
+
+function CreateRepresentativeForm() {
+  return (
+    <Form action="" className="flex flex-col gap-2">
+      <Input type="text" name="firstName" placeholder="First Name" required />
+      <Input type="text" name="lastName" placeholder="Last Name" required />
+      <Input type="email" name="email" placeholder="Email" required />
+      <Button type="submit">Save</Button>
+      <DrawerClose asChild>
+        <Button variant="outline">Cancel</Button>
+      </DrawerClose>
+    </Form>
   );
 }
