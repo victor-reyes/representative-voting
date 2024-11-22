f0dc3c44a930bd0eb3b17d9723e872ee3adca833
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-22 21:27:19 +0100

    Style RepresentativeItem component

--- src/app/representatives/page.tsx
+++ src/app/representatives/page.tsx
@@ -1,3 +1,5 @@
+import { Card } from "@/components/ui/card";
+
 const representatives: { id: number; firstName: string; lastName: string }[] = [
   { id: 1, firstName: "John", lastName: "Doe" },
   { id: 2, firstName: "Jane", lastName: "Smith" },
@@ -8,7 +10,7 @@ export default function Representatives() {
   return (
     <main>
       <div>Represenatives!</div>
-      <ul>
+      <ul className="space-y-2">
         {representatives.map((representative) => (
           <RepresentativeItem
             key={representative.id}
@@ -27,9 +29,11 @@ type Props = {
 export function RepresentativeItem({ representative }: Props) {
   return (
     <li>
-      <h2>
-        {representative.firstName} {representative.lastName}
-      </h2>
+      <Card className="px-4 py-2">
+        <h2>
+          {representative.firstName} {representative.lastName}
+        </h2>
+      </Card>
     </li>
   );
 }
