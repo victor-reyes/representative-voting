b968488252a7a050f7b87ddc6e709836eba22329
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-22 17:38:13 +0100

    Add mock data with petitions: {id,topic,isDone}[] and display them inside a list in /petions

--- src/app/petitions/page.tsx
+++ src/app/petitions/page.tsx
@@ -1,7 +1,21 @@
+const petitions = [
+  { id: 1, topic: "Free Ice Cream for Everyone!", isDone: false },
+  { id: 2, topic: "Higher Wages for Teachers", isDone: true },
+  { id: 3, topic: "More Nuclear Power Plants", isDone: true },
+];
+
 export default function Petitions() {
   return (
     <main>
-      <div>Petitions!</div>
+      <h1>Petitions!</h1>
+      <ul className="flex flex-col gap-2">
+        {petitions.map((petition) => (
+          <li key={petition.id} className="flex gap-4">
+            <h2>{petition.topic}</h2>
+            <p>{petition.isDone ? "Done" : "Ongoing"} </p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
