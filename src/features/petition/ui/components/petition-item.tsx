2efabfb2f25698419291b077b5419ac0d4acabd9
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-23 20:37:26 +0100

    Display alternatives in list in PetitionItem

--- src/features/petition/ui/components/petition-item.tsx
+++ src/features/petition/ui/components/petition-item.tsx
@@ -1,7 +1,14 @@
 import { Card } from "@/components";
 
 type Props = {
-  petition: { id: number; topic: string; description: string; isDone: boolean };
+  petition: {
+    id: number;
+    topic: string;
+    description: string;
+    alternatives: string[];
+    startTimestamp: Date;
+    isDone: boolean;
+  };
 };
 
 export function PetitionItem({ petition }: Props) {
@@ -11,9 +18,25 @@ export function PetitionItem({ petition }: Props) {
         <div>
           <h2 className="font-bold">{petition.topic}</h2>
           <p className="text-slate-600">{petition.description}</p>
+          <Alternatives alternatives={petition.alternatives} />
         </div>
         <div>{petition.isDone ? "Done" : "Ongoing"}</div>
       </Card>
     </li>
+  );
+}
+
+function Alternatives({ alternatives }: { alternatives: string[] }) {
+  return (
+    <div className="p-2">
+      <h3 className="font-serif">Alternatives:</h3>
+      <ul className="list-disc  ps-6">
+        {alternatives.map((alternative) => (
+          <li key={alternative} className="text-slate-600">
+            {alternative}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
