ffdec0e19052ee16c94db07ed4bd2da0de0a5c86
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-23 12:41:22 +0100

    Create a new representative in server action and revalidate path to display updated list

--- src/features/representative/actions.ts
+++ src/features/representative/actions.ts
@@ -2,13 +2,14 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { instance } from "./instance";
 
 export async function createRepresentativeAction(formData: FormData) {
   const firstName = formData.get("firstName") as string;
   const lastName = formData.get("lastName") as string;
   const email = formData.get("email") as string;
 
-  console.log(`Creating representative: ${firstName} ${lastName} (${email})`);
+  instance.service.create(firstName, lastName, email);
 
   revalidatePath("/representatives");
   redirect("/representatives");
