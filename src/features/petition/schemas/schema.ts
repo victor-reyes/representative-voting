49cba29283f94aabd19e42911220f603b3fab9b1
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-23 20:04:56 +0100

    Make timestamps to be integers

--- src/features/petition/schemas/schema.ts
+++ src/features/petition/schemas/schema.ts
@@ -1,13 +1,13 @@
 import { sql } from "drizzle-orm";
-import { integer, jsonb, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { integer, jsonb, pgTable, text } from "drizzle-orm/pg-core";
 
 export const petionsTable = pgTable("petitions", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: text().notNull(),
   description: text().notNull(),
   alternatives: jsonb().$type<string[]>().notNull(),
-  startTimestamp: timestamp()
+  startTimestamp: integer()
     .notNull()
     .default(sql`extract(epoch from now())`),
-  endTimestamp: timestamp(),
+  endTimestamp: integer(),
 });
