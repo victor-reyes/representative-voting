deaeae711325074bd7e53b74e074b5bea6c0fe11
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-23 21:27:39 +0100

    Add representatives table

--- src/features/representative/schemas/schema.ts
+++ src/features/representative/schemas/schema.ts
@@ -3,3 +3,11 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
   email: text().primaryKey(),
 });
+
+export const representativesTable = pgTable("representatives", {
+  email: text()
+    .primaryKey()
+    .references(() => usersTable.email),
+  firstName: text().notNull(),
+  lastName: text().notNull(),
+});
