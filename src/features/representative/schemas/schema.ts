1ce59e8fa07ffb57308d63de1beca0612439193e
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-23 21:29:39 +0100

    Add user votings table

--- src/features/representative/schemas/schema.ts
+++ src/features/representative/schemas/schema.ts
@@ -11,3 +11,13 @@ export const representativesTable = pgTable("representatives", {
   firstName: text().notNull(),
   lastName: text().notNull(),
 });
+
+export const userVotingTable = pgTable("user_voting", {
+  userEmail: text()
+    .primaryKey()
+    .references(() => usersTable.email),
+  representativeEmail: text()
+    .primaryKey()
+    .references(() => representativesTable.email),
+  timestamp: text().notNull(),
+});
