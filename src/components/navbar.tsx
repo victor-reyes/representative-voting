03d71a8859e034f88e64de5ca6473c689f0b392f
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-25 00:49:34 +0100

    Higlight link when it is displaying current page and slightly higlight on hover

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavItem = [
   { href: "/", label: "Home" },
@@ -7,11 +9,20 @@ const NavItem = [
 ];
 
 export function Navbar() {
+  const currentPath = usePathname();
+
   return (
-    <nav className="px-8 py-4 bg-slate-700 text-white sticky top-0">
+    <nav className="px-8 py-4 bg-slate-700 text-slate-300 sticky top-0">
       <ul className="flex gap-4">
         {NavItem.map((navItem) => (
-          <li key={navItem.href}>
+          <li
+            key={navItem.href}
+            className={`${
+              currentPath === navItem.href
+                ? "text-white underline"
+                : "hover:text-slate-200"
+            }`}
+          >
             <Link href={navItem.href}>{navItem.label}</Link>
           </li>
         ))}
