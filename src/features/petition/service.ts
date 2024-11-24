cebac81cb80cd0eb59340b7a2693039963c872d0
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 22:19:09 +0100

    Make get all function in petition services handle statistics (votes' count and who won)

--- src/features/petition/service.ts
+++ src/features/petition/service.ts
@@ -1,14 +1,36 @@
 import { Repository } from "./repository";
+import { calculateVotesFor, calculateWinLostDraw } from "./logic";
+import { representative } from "..";
 
 export function createService(repository: Repository) {
+  const representativeService = representative.service;
+
   return {
     async getAll() {
-      return (await repository.getAll()).map((petition) => {
+      const petitions = (await repository.getAll()).map((petition) => {
         return {
           ...petition,
           isDone: petition.endTimestamp ? true : false,
         };
       });
+
+      const petionsWithStats = await Promise.all(
+        petitions.map(async (petition) => {
+          const representatives =
+            await representativeService.getRepresentativesForPetition(
+              petition.id,
+              petition.startTimestamp,
+            );
+          const choicesWithVotes = petition.choices.map((choice) =>
+            calculateVotesFor(choice, representatives),
+          );
+          const choicesWithResult = calculateWinLostDraw(choicesWithVotes);
+
+          return { ...petition, representatives, choices: choicesWithResult };
+        }),
+      );
+
+      return petionsWithStats;
     },
 
     async create(
