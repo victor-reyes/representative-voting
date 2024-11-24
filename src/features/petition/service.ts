24a0ce73c74ea48e22a3d31fc6820dc7f25fc5d4
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 14:16:24 +0100

    Refactor petion service mapping

--- src/features/petition/service.ts
+++ src/features/petition/service.ts
@@ -3,25 +3,13 @@ import { Repository } from "./repository";
 export function createService(repository: Repository) {
   return {
     async getAll() {
-      return (await repository.getAll()).map(
-        ({
-          id,
-          topic,
-          description,
-          choices,
-          startTimestamp,
-          endTimestamp,
-        }) => {
-          return {
-            id,
-            topic,
-            description,
-            choices,
-            startTimestamp: new Date(startTimestamp * 1000),
-            isDone: endTimestamp ? true : false,
-          };
-        },
-      );
+      return (await repository.getAll()).map((petition) => {
+        return {
+          ...petition,
+          startTimestamp: new Date(petition.startTimestamp * 1000),
+          isDone: petition.endTimestamp ? true : false,
+        };
+      });
     },
 
     async create(topic: string, description: string, choices: string[]) {
