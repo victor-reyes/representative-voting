36fb8dcec73574ff08261f17cbd2fa8ecb22ebdd
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 13:00:17 +0100

    Let representative feature service access petition feature service getAll petions function

--- src/features/representative/feature.ts
+++ src/features/representative/feature.ts
@@ -1,8 +1,13 @@
+import { petition } from "../petition";
 import { createRepository } from "./repository";
 import { createService } from "./service";
 
 export function createFeature() {
   const repository = createRepository();
-  const service = createService(repository);
+  const service = createService({
+    ...repository,
+    getAllPetitions: petition.service.getAll,
+  });
+
   return { repository, service };
 }
--- src/features/representative/service.ts
+++ src/features/representative/service.ts
@@ -1,6 +1,20 @@
+import { calculateRepresentativesVotingPower } from "./logic";
 import { Repository } from "./repository";
 
-export function createService(repository: Repository) {
+export function createService(
+  repository: Repository & {
+    getAllPetitions: () => Promise<
+      {
+        id: number;
+        topic: string;
+        description: string;
+        alternatives: string[];
+        startTimestamp: Date;
+        isDone: boolean;
+      }[]
+    >;
+  },
+) {
   return {
     async getAll() {
       return await repository.getAll();
