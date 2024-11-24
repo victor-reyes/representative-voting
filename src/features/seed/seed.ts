ed05db721028fe015b507091dccdacd340fbd581
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 14:38:58 +0100

    Implement getFakeUsers(numberOfUsers) function that returns array of fake users of size of numberOfUsers with default 100

--- src/features/seed/seed.ts
+++ src/features/seed/seed.ts
@@ -1,9 +1,18 @@
 import { petition } from "../petition";
 import { representative } from "../representative";
+import { faker } from "@faker-js/faker";
 
 async function seed() {
   const petitionService = petition.service;
   const representativeService = representative.service;
+
+  const users = await getFakeUsers();
+}
+
+async function getFakeUsers(numberOfUsers = 1000) {
+  return Array.from({ length: numberOfUsers }).map(() => ({
+    email: faker.internet.email(),
+  }));
 }
 
 seed();
