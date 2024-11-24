b4b37350eea5645252e23ba7d8a9c156d0fa57f6
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-25 00:36:26 +0100

    Add to root page / an explanation that the page is under construction and provide links to /representatives and /petitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button, Explanation } from "@/components";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Explanation explanation="This page is under construction. Check back later or go to the following pages.">
+        <Button asChild>
+          <Link href="/representatives">Representatives</Link>
+        </Button>
+
+        <Button asChild>
+          <Link href="/petitions">Petitions</Link>
+        </Button>
+      </Explanation>
     </main>
   );
 }
