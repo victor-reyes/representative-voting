b8fffd664415eb043214112d8b3fe8270e4a0e9f
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 19:08:28 +0100

    Sort petions by start timestamp from most recent to oldest one

--- src/features/petition/repository.ts
+++ src/features/petition/repository.ts
@@ -1,10 +1,14 @@
 import { db } from "@/db";
 import { petitionsTable } from "./schemas/schema";
+import { desc } from "drizzle-orm";
 
 export function createRepository() {
   return {
     async getAll() {
-      return await db.select().from(petitionsTable);
+      return await db
+        .select()
+        .from(petitionsTable)
+        .orderBy(desc(petitionsTable.startTimestamp));
     },
 
     async create(topic: string, description: string, choices: string[], timestamp?: number) {
