326d0bb2d880a07881ef971a533ccac6f1f1dc0d
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 12:10:45 +0100

    Add Zero-case test that ensures empty list is return if there are no representatives

--- src/features/representative/logic.test.ts
+++ src/features/representative/logic.test.ts
@@ -1,7 +1,19 @@
 import { describe, it } from "node:test";
+import { deepEqual } from "node:assert/strict";
+import { calculateRepresentativesVotingPower } from "./logic";
 
-describe("Representatives voting power:", () => {
-  it("should return empty list if there are no representatives", () => {});
+describe("Representatives voting power:", async () => {
+  it("should return empty list if there are no representatives", async () => {
+    const userVotes = [
+      { timestamp: 1, representativeEmail: "email1", userEmail: "user1" },
+      { timestamp: 2, representativeEmail: "email2", userEmail: "user2" },
+      { timestamp: 3, representativeEmail: "email3", userEmail: "user3" },
+    ];
+
+    const result = await calculateRepresentativesVotingPower(userVotes, []);
+
+    deepEqual(result, []);
+  });
   it("should return list with representatives wwith voting power of zero if there are no votes", () => {});
   it("should be calculated based on the number of users' most recent votes", () => {});
 });
