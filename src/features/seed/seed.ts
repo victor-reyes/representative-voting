2eae91fd946a961e1c0d8d89da2f38f208819c78
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 14:21:33 +0100

    Set up seed feature

---
+++ src/features/seed/seed.ts
@@ -0,0 +1,9 @@
+import { petition } from "../petition";
+import { representative } from "../representative";
+
+async function seed() {
+  const petitionService = petition.service;
+  const representativeService = representative.service;
+}
+
+seed();
