b9f54fe0e92837f439e42c73a236a68b74a807a6
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 14:49:53 +0100

    Implement getFakeRepresentatives(users: { email: string }[],numberOfRepresentatives = 10) that randomly choses representatives out of array of users

--- src/features/seed/seed.ts
+++ src/features/seed/seed.ts
@@ -7,12 +7,32 @@ async function seed() {
   const representativeService = representative.service;
 
   const users = await getFakeUsers();
+  const representatives = await getFakeRepresentatives(users);
 }
 
 async function getFakeUsers(numberOfUsers = 1000) {
   return Array.from({ length: numberOfUsers }).map(() => ({
     email: faker.internet.email(),
   }));
+}
+
+async function getFakeRepresentatives(
+  users: { email: string }[],
+  numberOfRepresentatives = 10,
+) {
+  const representatives = Array.from({ length: numberOfRepresentatives }).map(
+    () => {
+      const user = users[Math.floor(Math.random() * users.length)];
+      return {
+        ...user,
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+      };
+    },
+  );
+
+  const uniqueRepresentatives = [...new Set(representatives)];
+  return uniqueRepresentatives;
 }
 
 seed();
