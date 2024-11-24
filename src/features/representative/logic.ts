d43706c8872767afce25d7330a2c5815a48643a7
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 19:54:37 +0100

    Refactor out getMostRecentVotes

--- src/features/representative/logic.ts
+++ src/features/representative/logic.ts
@@ -4,10 +4,7 @@ export async function calculateRepresentativesVotingPower(
   userVotes: (typeof userVotingTable.$inferSelect)[],
   representatives: (typeof representativesTable.$inferSelect)[],
 ) {
-  const mostRecentVotes = userVotes.filter(
-    (vote, index, array) =>
-      array.findIndex((v) => v.userEmail === vote.userEmail) === index,
-  );
+  const mostRecentVotes = getMostRecentVotes(userVotes);
 
   return representatives.map((representative) => {
     const votes = mostRecentVotes.filter(
@@ -16,4 +13,17 @@ export async function calculateRepresentativesVotingPower(
     const votingPower = votes.length;
     return { ...representative, votingPower };
   });
+}
+
+export function getMostRecentVotes(
+  userVotes: {
+    timestamp: number;
+    representativeEmail: string;
+    userEmail: string;
+  }[],
+) {
+  return userVotes.filter(
+    (vote, index, array) =>
+      array.findIndex((v) => v.userEmail === vote.userEmail) === index,
+  );
 }
