90863726bc6f59f3e72ef925eae037641367e069
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-24 12:02:30 +0100

    Set up tests for calculating voter power logic

---
+++ src/features/representative/logic.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from "node:test";
+
+describe("Representatives voting power:", () => {
+  it("should return empty list if there are no representatives", () => {});
+  it("should return list with representatives wwith voting power of zero if there are no votes", () => {});
+  it("should be calculated based on the number of users' most recent votes", () => {});
+});
